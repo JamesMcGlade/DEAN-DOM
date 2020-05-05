0a714ceea703e3b8bf2df75448e96c602e7b85c4
Code Nation Student <codenationstudent@codes-mbp.home>
2020-05-05 16:14:10 +0100

    Fixed code

---
+++ index.js
@@ -0,0 +1,17 @@
+const wrapper = document.getElementById('wrapper');
+
+let div = document.createElement('div');
+div.id = 'box';
+
+let text = document.createTextNode('JS');
+
+wrapper.appendChild(div);
+div.appendChild(text);
+
+
+
+// myDiv.classList.add('class1', 'class2');
+// myDiv.classList.toggle();
+// myDiv.classList.remove();
+
+console.log(div);
